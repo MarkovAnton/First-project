8c320c12ec46582256b8dfb3d46dc0b86e31d7d0
MarkovAnton <markov_anton2001@mail.ru>
2021-06-10 20:30:06 +0300

    add correlations.py

---
+++ correlations.py
@@ -0,0 +1,10 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def corr_data(train):
+    correlations_data = train.corr()['sales'].sort_values()
+    sns.heatmap(train.corr())
+    plt.title('Сorrelation for data')
+    plt.show()
+    return correlations_data
