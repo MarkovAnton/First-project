3da3a9c532214925c757d6e56e3d4e41d85b1be2
MarkovAnton <markov_anton2001@mail.ru>
2021-06-10 20:31:51 +0300

    add hisplot_for_data.py

---
+++ hisplot_for_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+
+def his_plot(train):
+    pl = pd.DataFrame()
+    pl['sales'] = train['sales']
+    sns.histplot(data=pl, x='sales', bins=40)
+    plt.title('Sales bar chart')
+    plt.show()
