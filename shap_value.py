43650a288109dc2f9bc093def7b288fb6fe3a0bf
MarkovAnton <markov_anton2001@mail.ru>
2021-06-10 20:34:04 +0300

    add shap_value.py

---
+++ shap_value.py
@@ -0,0 +1,9 @@
+import shap
+from matplotlib import pyplot as plt
+
+
+def shap_val(gb_model, train_X):
+    shap_values = shap.TreeExplainer(gb_model).shap_values(train_X)
+    print(shap_values.shape)
+    shap.summary_plot(shap_values, train_X)
+    plt.show()
