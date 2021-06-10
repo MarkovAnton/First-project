375eaeeeafa15c3a91b41f8d81e2103dcb2c6012
MarkovAnton <markov_anton2001@mail.ru>
2021-06-10 20:31:11 +0300

    add distribution_of_residuals.py

---
+++ distribution_of_residuals.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+
+def distrib_of_residual(residuals):
+    plt.hist(residuals, color='red', bins=20, edgecolor='black')
+    plt.xlabel('Error')
+    plt.ylabel('Count')
+    plt.title('Distribution of Residuals')
+    plt.show()
